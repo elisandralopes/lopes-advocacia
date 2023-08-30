2eb89a447d0e568640ebfdf3c9293f4111f37565
Elisandra Lopes <110205082+elisandralopes@users.noreply.github.com>
2023-08-29 20:04:13 -0400

    feat: criação do componente card-service

---
+++ src/components/card-service/card-service.js
@@ -0,0 +1,13 @@
+export default function Card(titulo, text) {
+    return /*html*/`
+    
+    <div class="service-card">
+        <p class="service-card__title">■ ${titulo}</p>
+        <p>${text}</p>
+    </div>`;
+    
+}
+
+
+
+
