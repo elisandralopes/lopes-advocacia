ad85e17794e88748365237ab2ba5ebcdaa56a2e7
Elisandra Lopes <110205082+elisandralopes@users.noreply.github.com>
2023-09-07 20:20:07 -0400

    feat: criação da api

--- api/app.js
+++ api/app.js
@@ -2,15 +2,35 @@ const express = require('express');
 const app = express();
 const headerJson = require('./data/header.json');
 const mainJson = require('./data/main.json');
+const lawyerJson = require('./data/lawyer.json');
+const servicesJson = require('./data/services.json');
+const contactJson = require('./data/contact.json');
+const footerJson = require('./data/footer.json');
 
 app.get('/header', (req, res) => {
-    return res.json(headerJson);
+  return res.json(headerJson);
 });
 
 app.get('/main', (req, res) => {
-    return res.json(mainJson);
+  return res.json(mainJson);
+});
+
+app.get('/lawyer', (req, res) => {
+  return res.json(lawyerJson);
+});
+
+app.get('/services', (req, res) => {
+  return res.json(servicesJson);
+});
+
+app.get('/contact', (req, res) => {
+  return res.json(contactJson);
+});
+
+app.get('/footer', (req, res) => {
+  return res.json(footerJson);
 });
 
 app.listen(3000, () => {
-    console.log('start api in port 3000');
+  console.log('start api in port 3000');
 });
