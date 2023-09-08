a0110c9d452b482d60daf5449fbaf56afde5b3a0
Elisandra Lopes <110205082+elisandralopes@users.noreply.github.com>
2023-09-07 20:19:25 -0400

    fix: import css retirado, await added

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 import './src/global.css';
-import './src/components/logo/logo.css';
-import './src/components/button/button.css';
-import './src/components/card-service/card-service.css';
-import './src/components/card-contacts/card-contacts.css';
-import './src/components/form/form.css';
-import './src/header/header.css';
-import './src/main/main.css';
-import './src/services/services.css';
-import './src/lawyer/lawyer.css';
-import './src/contact/contact.css';
-import './src/footer/footer.css';
 
 import Header from './src/header/header';
 import Main from './src/main/main';
@@ -18,21 +7,20 @@ import Lawyer from './src/lawyer/lawyer';
 import Contact from './src/contact/contact';
 import Footer from './src/footer/footer';
 
-async function App()  {
-    const header = new Header();
-    const main = new Main();
-    const services = new Services();
-    const lawyer = new Lawyer();
-    const contact = new Contact();
-    const footer = new Footer();
-    
-    
-    document.querySelector('#header').innerHTML = await header.render();
-    document.querySelector('#main').innerHTML = await main.render();
-    document.querySelector('#services').innerHTML = services.render();
-    document.querySelector('#lawyer').innerHTML = lawyer.render();
-    document.querySelector('#contact').innerHTML = contact.render();
-    document.querySelector('#footer').innerHTML = footer.render();
+async function App() {
+  const header = new Header();
+  const main = new Main();
+  const services = new Services();
+  const lawyer = new Lawyer();
+  const contact = new Contact();
+  const footer = new Footer();
+
+  document.querySelector('#header').innerHTML = await header.render();
+  document.querySelector('#main').innerHTML = await main.render();
+  document.querySelector('#services').innerHTML = await services.render();
+  document.querySelector('#lawyer').innerHTML = await lawyer.render();
+  document.querySelector('#contact').innerHTML = await contact.render();
+  document.querySelector('#footer').innerHTML = await footer.render();
 }
 
 App();
